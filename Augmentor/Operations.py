95adc6ebebb7580550cd274a37c2fbe77185eff4
ricky-kiva <ricky.kiva@gmail.com>
2024-07-02 23:25:27 +0700

    feat(operations): keep `Distort` as random

--- Augmentor/Operations.py
+++ Augmentor/Operations.py
@@ -1453,8 +1453,8 @@ class Distort(Operation):
                 polygon_indices.append([i, i + 1, i + horizontal_tiles, i + 1 + horizontal_tiles])
 
         for a, b, c, d in polygon_indices:
-            dx = self.magnitude
-            dy = self.magnitude
+            dx = random.randint(-self.magnitude, self.magnitude)
+            dy = random.randint(-self.magnitude, self.magnitude)
 
             x1, y1, x2, y2, x3, y3, x4, y4 = polygons[a]
             polygons[a] = [x1, y1,
